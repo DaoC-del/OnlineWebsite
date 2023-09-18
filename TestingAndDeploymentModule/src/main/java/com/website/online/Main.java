971b3d156b61953688397b92f5957c084625fd4f
ヨ <550164003@163.com>
2023-09-19 07:49:08 +0900

    userManagementModule

--- TestingAndDeploymentModule/src/main/java/com/website/online/Main.java
+++
@@ -1,7 +0,0 @@
-package com.website.online;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- UserManagementModule/src/main/java/com/website/online/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.website.online.mapper;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.website.online.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-
-
-public interface UserMapper extends BaseMapper<User> {}
